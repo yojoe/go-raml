d7d121dcc4cfbe3a1423952e2903c29971967e61
Iwan BK <iwanbk@gmail.com>
2016-02-04 13:37:29 +0700

    remove empty resource test

--- commands/server_resources_gen_test.go
+++ commands/server_resources_gen_test.go
@@ -13,30 +13,7 @@ func testLoadFile(filename string) (string, error) {
 	return string(b), err
 }
 func TestResource(t *testing.T) {
-	Convey("test resource generator", t, func() {
-		Convey("Empty resource", func() {
-			rs := map[string]raml.Resource{
-				"/gists": raml.Resource{
-					DisplayName: "Gists",
-					Nested: map[string]*raml.Resource{
-						"/public": &raml.Resource{
-							DisplayName: "Public Gists",
-						},
-					},
-				},
-			}
-			err := ServerResourcesGen(rs, "./tmp")
-			So(err, ShouldBeNil)
-
-			// check interface file
-			s, err := testLoadFile("./tmp/gists_if.go")
-			So(err, ShouldBeNil)
-
-			tmpl, err := testLoadFile("./fixtures/gists_if.go")
-			So(err, ShouldBeNil)
-			So(s, ShouldEqual, tmpl)
-		})
-
+	Convey("resource generator", t, func() {
 		Convey("simple resource", func() {
 			apiDef, err := raml.ParseFile("./fixtures/resource_deliveries.raml")
 			So(err, ShouldBeNil)
