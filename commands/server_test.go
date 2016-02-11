20f1a5e2942f2edef11a83998e6f3f5b156b2e8e
Iwan BK <iwanbk@gmail.com>
2016-02-11 10:25:20 +0700

    fix server test

--- commands/server_test.go
+++ commands/server_test.go
@@ -1,13 +1,50 @@
 package commands
 
 import (
+	"os"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestServerGeneration(t *testing.T) {
-	cmd := ServerCommand{}
-	err := cmd.Execute()
-	assert.NoError(t, err)
+	Convey("test command server generattion", t, func() {
+		Convey("Test run server command using go language", func() {
+			cmd := ServerCommand{
+				Dir:      "./test_server_command",
+				RamlFile: "./fixtures/server/user_api/api.raml",
+			}
+			err := cmd.Execute()
+			So(err, ShouldBeNil)
+
+			// check users api implementation
+			s, err := testLoadFile("./test_server_command/users_api.go")
+			So(err, ShouldBeNil)
+
+			tmpl, err := testLoadFile("./fixtures/server/user_api/users_api.txt")
+			So(err, ShouldBeNil)
+			So(s, ShouldEqual, tmpl)
+
+			// check user interface
+			s, err = testLoadFile("./test_server_command/users_if.go")
+			So(err, ShouldBeNil)
+
+			tmpl, err = testLoadFile("./fixtures/server/user_api/users_if.txt")
+			So(err, ShouldBeNil)
+			So(s, ShouldEqual, tmpl)
+
+			// check main file
+			s, err = testLoadFile("./test_server_command/main.go")
+			So(err, ShouldBeNil)
+
+			tmpl, err = testLoadFile("./fixtures/server/user_api/main.txt")
+			So(err, ShouldBeNil)
+			So(s, ShouldEqual, tmpl)
+		})
+
+		Reset(func() {
+			//cleanup
+			os.RemoveAll("./test_server_command")
+		})
+	})
 }
