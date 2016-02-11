ca09a17bd77304305f34b3d468ef1c0bd7bde2f4
Iwan BK <iwanbk@gmail.com>
2016-02-11 09:38:18 +0700

    integrate client generation to CLI

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/codegangsta/cli"
 )
 
-//Version
+//Version define software version
 var Version = "0.1-Dev"
 
 //ApplicationName is the name of the application
@@ -65,6 +65,18 @@ func main() {
 					Value:       "go",
 					Usage:       "Language to construct a client for",
 					Destination: &clientCommand.Language,
+				},
+				cli.StringFlag{
+					Name:        "dir",
+					Value:       ".",
+					Usage:       "target directory",
+					Destination: &clientCommand.Dir,
+				},
+				cli.StringFlag{
+					Name:        "ramlfile",
+					Value:       ".",
+					Usage:       "Source raml file",
+					Destination: &clientCommand.RamlFile,
 				},
 			},
 			Action: func(c *cli.Context) {
