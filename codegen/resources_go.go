a2b5885694d34ff708a0199481c97b5dcd4c41f0
Iwan BK <iwanbk@gmail.com>
2016-03-30 17:19:42 +0700

    go custom type validator : fix unit test

--- codegen/resources_go.go
+++ codegen/resources_go.go
@@ -15,7 +15,6 @@ type goResource struct {
 	*resourceDef
 	WithMiddleware bool // this resource need middleware, we need to import github/justinas/alice
 	NeedJSON       bool // if true, the API implementation to import encoding/json package
-	NeedValidator  bool // this resource need validator
 }
 
 // generate interface file of a resource
@@ -54,11 +53,6 @@ func (gr *goResource) setImport() {
 		// if there is request/response body, then it needs to import encoding/json
 		if gm.RespBody != "" || gm.ReqBody != "" {
 			gr.NeedJSON = true
-		}
-
-		/// if there is request body, we need to import validator
-		if gm.ReqBody != "" {
-			gr.NeedValidator = true
 		}
 
 		// if has middleware, we need to import middleware lib
