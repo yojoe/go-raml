1818f8e28175df27f125bb9289ce39e09bc416f9
Iwan BK <iwanbk@gmail.com>
2016-03-31 15:42:56 +0700

    python validator : fix test

--- codegen/fixtures/struct/UsersPostReqBody.py
+++ codegen/fixtures/struct/UsersPostReqBody.py
@@ -1,13 +1,19 @@
 
 from flask_wtf import Form
-from wtforms.validators import DataRequired, Length, Regexp, NumberRange
-from wtforms import TextField, FormField, IntegerField, FloatField, FileField, BooleanField, DateField
+from wtforms.validators import DataRequired, Length, Regexp, NumberRange, required
+from wtforms import TextField, FormField, IntegerField, FloatField, FileField, BooleanField, DateField, FieldList
 
 class UsersPostReqBody(Form):
     
     ID = TextField(validators=[DataRequired(message=""), Length(min=4, max=8)])
     
+    
     age = IntegerField(validators=[DataRequired(message=""), NumberRange(min=16, max=100)])
     
+    
+    
+    grades = FieldList(IntegerField('grades', [required()]), min_entries=2,max_entries=5)
+    
     item = TextField(validators=[DataRequired(message=""), Length(min=2), Regexp(regex="^[a-zA-Z]+$")])
+    
     
--- codegen/fixtures/struct/ValidationString.py
+++ codegen/fixtures/struct/ValidationString.py
@@ -1,9 +1,10 @@
 
 from flask_wtf import Form
-from wtforms.validators import DataRequired, Length, Regexp, NumberRange
-from wtforms import TextField, FormField, IntegerField, FloatField, FileField, BooleanField, DateField
+from wtforms.validators import DataRequired, Length, Regexp, NumberRange, required
+from wtforms import TextField, FormField, IntegerField, FloatField, FileField, BooleanField, DateField, FieldList
 
 class ValidationString(Form):
     
     name = TextField(validators=[DataRequired(message=""), Length(min=8, max=40)])
+    
     
