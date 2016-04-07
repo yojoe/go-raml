c86b43dcbe96de14a22b68c710199e2786cecebd
kristof de spiegeleer <kristof@incubaid.com>
2016-04-07 10:03:20 +0400

    docs

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+go get -u github.com/tools/godep
+go get -u github.com/jteeuwen/go-bindata/...
+go get -u github.com/Jumpscale/go-raml
+cd $GOPATH/src/github.com/Jumpscale/go-raml
+sh build.sh
+
