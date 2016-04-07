d25c5fe2c403cc4e66f98a5a8db622c69c0b5001
kristof de spiegeleer <kristof@incubaid.com>
2016-04-07 11:16:37 +0400

    docs

--- examples/python/do.sh
+++ examples/python/do.sh
@@ -1,2 +1,3 @@
 mkdir -p server
 go-raml server -l python --dir ./server --ramlfile api.raml
+go-raml client -l python --dir ./client --ramlfile api.raml
---
+++ examples/python/testclient.py
@@ -0,0 +1,7 @@
+
+from client.client import *
+
+cl=Client()
+
+cl.url="http://127.0.0.1:5000/"
+
