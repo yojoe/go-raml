fe0d84b0924f4d94c67c39fec47c0bba2445b2b1
Iwan BK <iwanbk@gmail.com>
2016-10-21 14:10:22 +0700

    add comment to generated code

--- codegen/nim/fixtures/resource/deliveries_api.nim
+++ codegen/nim/fixtures/resource/deliveries_api.nim
@@ -3,26 +3,31 @@ import jester, marshal
 
 
 proc deliveriesGet*(req: PRequest) : tuple[code: HttpCode, content: string] =
+  # Get a list of deliveries
   
   let respBody = ""
   result = (code: Http200, content: respBody)
 
 proc deliveriesPost*(req: PRequest) : tuple[code: HttpCode, content: string] =
+  # Create/request a new delivery
   
   let respBody = ""
   result = (code: Http200, content: respBody)
 
 proc getDeliveriesByDeliveryID*(deliveryId: string, req: PRequest) : tuple[code: HttpCode, content: string] =
+  # Get information on a specific delivery
   
   let respBody = ""
   result = (code: Http200, content: respBody)
 
 proc deliveriesByDeliveryIdPatch*(deliveryId: string, req: PRequest) : tuple[code: HttpCode, content: string] =
+  # Update the information on a specific delivery
   
   let respBody = ""
   result = (code: Http200, content: respBody)
 
 proc deliveriesByDeliveryIdDelete*(deliveryId: string, req: PRequest) : tuple[code: HttpCode, content: string] =
+  # Cancel a specific delivery
   
   let respBody = ""
   result = (code: Http200, content: respBody)
