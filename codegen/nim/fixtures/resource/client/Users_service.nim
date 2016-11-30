a29dfde79d5c12cd54771311a22cd534cbbe56c7
Iwan BK <iwanbk@gmail.com>
2016-11-30 17:57:47 +0700

    Fix #190: add query params support to Nim client.

--- codegen/nim/fixtures/resource/client/Users_service.nim
+++ codegen/nim/fixtures/resource/client/Users_service.nim
@@ -1,4 +1,4 @@
-import marshal
+import marshal, tables
 import client_struct
 
 import City
@@ -13,31 +13,31 @@ proc UsersSrv*(c : Client) : Users_service  =
   return Users_service(client:c, name:c.baseURI)
 
 
-proc GetUsers*(srv: Users_service) : usersGetRespBody =
-  let resp = srv.client.request("/users", "GET")
+proc GetUsers*(srv: Users_service, queryParams: Table[string, string] = initTable[string, string]()) : usersGetRespBody =
+  let resp = srv.client.request("/users", "GET", queryParams=queryParams)
   return to[usersGetRespBody](resp.body)
 
-proc usersPost*(srv: Users_service, reqBody: City) : City =
-  let resp = srv.client.request("/users", "POST", $$reqBody)
+proc usersPost*(srv: Users_service, reqBody: City, queryParams: Table[string, string] = initTable[string, string]()) : City =
+  let resp = srv.client.request("/users", "POST", $$reqBody, queryParams=queryParams)
   return to[City](resp.body)
 
-proc OptionsUsers*(srv: Users_service) : string =
-  let resp = srv.client.request("/users", "OPTIONS")
+proc OptionsUsers*(srv: Users_service, queryParams: Table[string, string] = initTable[string, string]()) : string =
+  let resp = srv.client.request("/users", "OPTIONS", queryParams=queryParams)
   return to[string](resp.body)
 
-proc GetUserByID*(srv: Users_service, userId: string) : City =
-  let resp = srv.client.request("/users/"&userId, "GET")
+proc GetUserByID*(srv: Users_service, userId: string, queryParams: Table[string, string] = initTable[string, string]()) : City =
+  let resp = srv.client.request("/users/"&userId, "GET", queryParams=queryParams)
   return to[City](resp.body)
 
-proc usersByUserIdDelete*(srv: Users_service, userId: string) : string =
-  let resp = srv.client.request("/users/"&userId, "DELETE")
+proc usersByUserIdDelete*(srv: Users_service, userId: string, queryParams: Table[string, string] = initTable[string, string]()) : string =
+  let resp = srv.client.request("/users/"&userId, "DELETE", queryParams=queryParams)
   return to[string](resp.body)
 
-proc usersByUserIdAddressPost*(srv: Users_service, reqBody: usersuserIdaddressPostReqBody, userId: string) : usersuserIdaddressPostRespBody =
-  let resp = srv.client.request("/users/"&userId&"/address", "POST", $$reqBody)
+proc usersByUserIdAddressPost*(srv: Users_service, reqBody: usersuserIdaddressPostReqBody, userId: string, queryParams: Table[string, string] = initTable[string, string]()) : usersuserIdaddressPostRespBody =
+  let resp = srv.client.request("/users/"&userId&"/address", "POST", $$reqBody, queryParams=queryParams)
   return to[usersuserIdaddressPostRespBody](resp.body)
 
-proc usersByUserIdAddressFolderAddressIdTestAddressId2Get*(srv: Users_service, addressId: string, addressId2: string, userId: string) : seq[address] =
-  let resp = srv.client.request("/users/"&userId&"/address/folder"&addressId&"test"&addressId2, "GET")
+proc usersByUserIdAddressFolderAddressIdTestAddressId2Get*(srv: Users_service, addressId: string, addressId2: string, userId: string, queryParams: Table[string, string] = initTable[string, string]()) : seq[address] =
+  let resp = srv.client.request("/users/"&userId&"/address/folder"&addressId&"test"&addressId2, "GET", queryParams=queryParams)
   return to[seq[address]](resp.body)
 
--- docs/tutorial/nim/client/goraml/client_goraml.nim
+++ docs/tutorial/nim/client/goraml/client_goraml.nim
@@ -1,11 +1,11 @@
-import httpclient, strutils
+import httpclient, strutils, tables
 
 type
   Client* = object
     baseURI*: string
     hc: HttpClient
 
-const defaultBaseURI = "http://172.20.10.10:5000"
+const defaultBaseURI = "http://localhost:5000"
 
 proc newClient*(baseURI = defaultBaseURI): Client =
   # creates new client
@@ -17,8 +17,28 @@ proc setAuthHeader*(c: Client, value: string) =
   c.hc.headers = newHttpHeaders({ "Content-Type": "application/json" })
   c.hc.headers.add("Authorization", value)
 
-proc request*(c: Client, endpoint: string, httpMethod = "GET", body = ""): httpclient.Response =
-  var uri: string = endpoint
-  if not uri.startsWith("http"):
-    uri = c.baseURI & uri
-  return c.hc.request(uri, httpMethod, body)
+proc addQueryParams(url: string, queryParams: Table) : string =
+  # add query params to the request URL
+  result = url
+  if len(queryParams) == 0:
+    return
+
+  var qp: seq[string] = @[]
+  for k,v  in queryParams.pairs():
+    qp.add($k & "=" & $v)
+
+  var sep: string = "?"
+
+  if url.find("?") > 0:
+    sep = "&"
+
+  result = url & sep & qp.join("&")
+
+
+proc request*(c: Client, endpoint: string, httpMethod = "GET", body = "", queryParams: Table[string, string] = initTable[string, string]()): httpclient.Response =
+  var url: string = endpoint
+  if not url.startsWith("http"):
+    url = c.baseURI & url
+
+  url = addQueryParams(url, queryParams)
+  return c.hc.request(url, httpMethod, body)
