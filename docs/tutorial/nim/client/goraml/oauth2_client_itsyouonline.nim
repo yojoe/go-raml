ad193698789338b3fd445441e788a288d5892b05
Iwan Budi Kusnanto <iwanbk@gmail.com>
2016-12-01 06:46:54 +0000

    Improve oauth2 Nim client to use new nim base client.

--- docs/tutorial/nim/client/goraml/oauth2_client_itsyouonline.nim
+++ docs/tutorial/nim/client/goraml/oauth2_client_itsyouonline.nim
@@ -1,26 +1,19 @@
-import strutils
+import strutils, tables
 
 import client_goraml
 
 let baseUri = "https://itsyou.online/v1/oauth/access_token?response_type=id_token"
 proc getAccessToken*(c: Client, clientID: string, clientSecret: string, scopes: openArray[string], auds: openArray[string]):string =
-  var q: seq[string]
-
-  q = @[]
-  q.add("grant_type=client_credentials")
-  q.add("client_id=" & clientID)
-  q.add("client_secret=" & clientSecret)
+  var qp: Table[string, string] = {
+    "grant_type": "client_credentials",
+    "client_id": clientID,
+    "client_secret": clientSecret
+  }.toTable
 
   if scopes.len > 0:
-    q.add("scope=" & scopes.join(","))
+    qp["scope"] = scopes.join(",")
 
   if auds.len > 0:
-    q.add("aud=" & auds.join(","))
+    qp["aud"] = auds.join(",")
 
-  var sep: string = "?"
-
-  if baseUri.find("?") > 0:
-    # there is already ? in the uri, we need to append "&" instead
-    sep = "&"
-
-  return c.request(baseUri & sep & q.join("&") , "POST").body
+  return c.request(baseUri, "POST", queryParams=qp).body
