cd8b69d1c8a5b456b231a8823afee451e8f5e953
Iwan BK <iwanbk@gmail.com>
2017-02-17 17:19:13 +0700

    Fix #217:escape field which use Nim keyword as name.

--- codegen/nim/field.go
+++ codegen/nim/field.go
@@ -4,11 +4,30 @@ import (
 	"github.com/Jumpscale/go-raml/raml"
 )
 
+var (
+	// list of nim keywords which need to escaped
+	// it only listed keywords that commonly used as field name
+	nimKeywords = map[string]bool{
+		"type":   true,
+		"method": true,
+	}
+)
+
 // field represents a Nim object field
 type field struct {
 	Name string // field name
 	Type string // field type
 	Enum *enum
+}
+
+// EscapedName produces escaped name of Nim keyword
+// or return as is if it is not Nim keyword
+func (f field) EscapedName() string {
+	name := f.Name
+	if _, ok := nimKeywords[name]; !ok {
+		return name
+	}
+	return "`" + name + "`"
 }
 
 func newField(objName string, prop raml.Property) field {
