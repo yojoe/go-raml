5d186b356fccc60bbf16198c9bb1bc6607967631
Iwan BK <iwanbk@gmail.com>
2017-02-20 14:54:15 +0700

    Add comment to Nim client service

--- codegen/nim/fixtures/resource/client/Users_service.nim
+++ codegen/nim/fixtures/resource/client/Users_service.nim
@@ -18,30 +18,51 @@ proc UsersSrv*(c : Client) : Users_service  =
 
 
 proc GetUsers*(srv: Users_service, queryParams: Table[string, string] = initTable[string, string]()) : usersGetRespBody =
+  # get users.# This method will be return list user.# Use it wisely.
+  # It calls GET /users endpoint.
+
   let resp = srv.client.request("/users", "GET", queryParams=queryParams)
   return to[usersGetRespBody](resp.body)
 
 proc usersPost*(srv: Users_service, reqBody: City, queryParams: Table[string, string] = initTable[string, string]()) : City =
+  # create users
+  # It calls POST /users endpoint.
+
   let resp = srv.client.request("/users", "POST", $$reqBody, queryParams=queryParams)
   return to[City](resp.body)
 
 proc OptionsUsers*(srv: Users_service, queryParams: Table[string, string] = initTable[string, string]()) : string =
+  
+  # It calls OPTIONS /users endpoint.
+
   let resp = srv.client.request("/users", "OPTIONS", queryParams=queryParams)
   return to[string](resp.body)
 
 proc GetUserByID*(srv: Users_service, userId: string, queryParams: Table[string, string] = initTable[string, string]()) : City =
+  # get id
+  # It calls GET /users/{userId} endpoint.
+
   let resp = srv.client.request("/users/"&userId, "GET", queryParams=queryParams)
   return to[City](resp.body)
 
 proc usersByUserIdDelete*(srv: Users_service, userId: string, queryParams: Table[string, string] = initTable[string, string]()) : string =
+  
+  # It calls DELETE /users/{userId} endpoint.
+
   let resp = srv.client.request("/users/"&userId, "DELETE", queryParams=queryParams)
   return to[string](resp.body)
 
 proc usersByUserIdAddressPost*(srv: Users_service, reqBody: usersuserIdaddressPostReqBody, userId: string, queryParams: Table[string, string] = initTable[string, string]()) : usersuserIdaddressPostRespBody =
+  
+  # It calls POST /users/{userId}/address endpoint.
+
   let resp = srv.client.request("/users/"&userId&"/address", "POST", $$reqBody, queryParams=queryParams)
   return to[usersuserIdaddressPostRespBody](resp.body)
 
 proc usersByUserIdAddressFolderAddressIdTestAddressId2Get*(srv: Users_service, addressId: string, addressId2: string, userId: string, queryParams: Table[string, string] = initTable[string, string]()) : seq[address] =
+  # get address id
+  # It calls GET /users/{userId}/address/folder{addressId}test{addressId2} endpoint.
+
   let resp = srv.client.request("/users/"&userId&"/address/folder"&addressId&"test"&addressId2, "GET", queryParams=queryParams)
   return to[seq[address]](resp.body)
 
