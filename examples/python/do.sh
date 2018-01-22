0988bbcacd22311718a3ec35b73339c0775decdf
Iwan Budi Kusnanto <ibk@labhijau.net>
2018-01-22 09:10:28 +0700

    remove outdated examples dir, use tutorial dir instead.

--- examples/python/do.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p server
-go-raml server -l python --dir ./server --ramlfile api.raml
-go-raml client -l python --dir ./client --ramlfile api.raml
--- examples/python/testclient.py
+++
@@ -1,7 +0,0 @@
-
-from client.client import *
-
-cl=Client()
-
-cl.url="http://127.0.0.1:5000/"
-
